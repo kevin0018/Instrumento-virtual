d20cd527f762a419d259d8f0c02d537bba6b4232
katerincolin <katerincolin25@gmail.com>
2025-05-16 11:03:12 +0200

    style header

---
+++ assets/js/header.js
@@ -0,0 +1,27 @@
+
+
+/* Header */
+function openMenu() {
+    document.getElementById('sidebar').classList.add('active');
+  }
+  
+  function closeMenu() {
+    document.getElementById('sidebar').classList.remove('active');
+  }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
